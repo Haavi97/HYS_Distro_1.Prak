2cfa120af7d76c3050e04bbb4c244461d4f70ed9
fortin <fortin@ttu.ee>
2021-01-31 16:54:10 +0200

    remove_ip function working

--- ipa_testing.py
+++ ipa_testing.py
@@ -3,14 +3,14 @@ from ipaddress import add_ip, remove_ip, ip_is_in_file
 
 
 if __name__ == '__main__':
+    erase_ipa_file()
     create_ipa_file()
     add_ip('192.168.1.1')
-    add_ip('192.168.1.1')
+    # add_ip('192.168.1.1')
     add_ip('192.168.1.2')
     add_ip('192.168.1.3')
     remove_ip('192.168.1.1')
     ip_is_in_file('192.168.1.1')
     add_ip('192.168.1.4')
     add_ip('192.168.1.1')
-    ip_is_in_file('192.168.1.3')
-    erase_ipa_file()
+    # ip_is_in_file('192.168.1.3')
--- ipaddress.py
+++ ipaddress.py
@@ -11,8 +11,8 @@ def create_ipa_file(file=default_path):
         if dirname not in os.listdir():
             os.mkdir(dirname)
         # TODO! Check first if file already exists
-        fd = open(file, 'r')
-        os.close(fd)
+        fd = open(file, 'x')
+        fd.close()
         print_wl('Succeed creating ipa_file')
     except:
         print_wl_error('Some error ocurred trying to create ipa_file')
@@ -43,27 +43,29 @@ def add_ip(ip_address, file=default_path):
 def remove_ip(ip_address, file=default_path):
     try:
         with open(default_path, 'r+') as fd:
-            if fd.read() == ip_address:
-                fd.seek(0)
-                fd.write('')
-                fd.truncate()
+            lines = fd.readlines()
+            fd.seek(0)
+            for line in lines:
+                if line.strip('\n') != ip_address:
+                    fd.write(line)
+            fd.truncate()
     except FileNotFoundError:
         print_wl_error('Trying to remove ipa but no file found')
 
 
 def ip_is_in_file(ip_address, file=default_path):
     try:
-        fd = open(default_path, "r")
-        lines = fd.readlines()
-        for line in lines:
-            if ip_address == line.strip("\n"):
-                fd.close()
-                return True
-        print_wl('Ip address not found')
-        fd.close()
+        with open(default_path, "r") as fd:
+            lines = fd.readlines()
+            for line in lines:
+                if ip_address == line.strip("\n"):
+                    fd.close()
+                    return True
+            print_wl('Ip address not found')
         return False
     except FileNotFoundError:
         print_wl_error('Searching ipa but no file found')
 
 
 # TODO! Check ip address validity
+# TODO! Add verbosity to messages
