dc942ff18a97c0d8156131c8a7aaf6d7145f0f40
Artur Kerb <arkerb@taltech.ee>
2021-03-21 11:50:39 +0200

    Fixed ini.py file

---
+++ server-client/ini.py
@@ -0,0 +1,30 @@
+import os
+from sys import argv
+from time import sleep
+
+class Ini:
+
+  def __init__(self):
+    self.path = os.getcwd() + os.sep
+    self.file_path = self.path + 'users' + os.sep + 'conf.txt'
+    self.read_ips(self.file_path)
+  
+  def read_ips(self, path):
+    with open(path) as fh:
+      fstring = fh.readlines()
+    
+    ports = []
+    for line in fstring:
+      ip = line.strip().split(':')
+      ports.append(ip[1])
+    
+    self.create_terminals(ports)
+  
+  def create_terminals(self, ports):
+    for port in  range(len(ports)):
+      command = 'python user.py ' + str(ports[port]) +  ' user' + str(port + 1)
+      os.system("start cmd /k " + command)
+      sleep(1)
+
+if __name__ == '__main__':
+  ini = Ini()
